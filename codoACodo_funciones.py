12857a0cc772fd15def2bbdccd3a5bbf188c749b
alanpirotta <alanupirotta@gmail.com>
2021-11-13 22:27:54 -0300

    Algunos ejercicios más de codo a codo

--- codoACodo_funciones.py
+++ codoACodo_funciones.py
@@ -110,8 +110,7 @@ def cambio(costo, pagado):
             billete1 += 1
             vuelto -= 1
     return print(f'Su vuelto es ${costo-pagado}. Dar de vuelto {billete500} billetes de $500, {billete100} billetes de $100, {billete50} billetes de $50, {billete20} billetes de $20, {billete10} billetes de $10, {billete5} billetes de $5 y {billete1} billetes de $1')
-    
-    
+   
 resultado= cambio(500,317)
 '''
 #Funcionó, pero quiero probar con listas y un eliminar cuando son 0 billetes
@@ -172,5 +171,113 @@ patronAumentaEnDos(10)
 '''
 Crear una función lambda que devuelva el cuadrado de un valor recibido cómo parámetro. Desarrollar además un programa para verificar el comportamiento de la función.
 '''
+'''
 cuadrado = lambda x: x**2
-cuadrado(4)
+print(cuadrado(12))
+'''
+
+'''
+Crear una función lambda para comprobar si un número es par o impar. Desarrollar además un programa para verificar el comportamiento de la función.
+'''
+'''
+par = lambda x: True if x%2 == 0 else False
+print(par(1126457892010))
+'''
+
+'''
+7) Crear una lista con los cuadrados de los números entre 1 y N (ambos incluidos), donde N se ingresa desde el teclado. Luego se solicita imprimir los últimos 10 valores de la lista.
+'''
+
+'''
+def listaCuadrados(N):
+    listaCuadrados=[]
+    for i in range(1,N+1):
+        listaCuadrados.append(i**2)
+    return listaCuadrados
+
+def imprimirUltimos10(lista):
+    ultimosDiez = []
+    for i in range(-10,-1):
+        ultimosDiez.append(lista[i])
+    print('Los últimos diez números de la lista son:', ultimosDiez)
+
+imprimirUltimos10(listaCuadrados(20))
+'''
+
+'''
+8) Eliminar de una lista de palabras que se encuentren en una segunda lista. Imprimir la lista original, la lista de palabras a eliminar y la lista resultante.
+'''
+
+'''
+lista = ["hola", "chau", "Alan", "Pedro", "Ana", "Daniela", "Carlos"]
+print(lista)
+listaPalabrasAEliminar = ["Alan", "chau", "Juan", "Ana"]
+print(listaPalabrasAEliminar)
+for element in listaPalabrasAEliminar:
+    if element in lista:    #Si no hago esto, tira error con palabras que no están
+        lista.remove(element) 
+print(lista)
+'''
+
+#Tiene un conflicto si la lista original tiene duplicados, sólo elimina el primero.
+'''
+#alternativa
+lista = ["hola", "chau", "Alan", "Pedro", "Ana", "Daniela", "Carlos"]
+print(lista)
+listaPalabrasAEliminar = ["Alan", "chau", "Juan", "Ana"]
+print(listaPalabrasAEliminar)
+for element in lista:
+    print(f'Principio del loop de {element}')
+    if element in listaPalabrasAEliminar:   
+        print(f'el {element} esta para eliminar')
+        lista.remove(element) 
+    print(f'Final del loop de {element}')
+print(lista)
+'''
+
+'''
+9) Escribir una función que reciba una lista como parámetro y devuelva True si la lista está ordenada en forma ascendente o False en caso contrario. Por ejemplo, ordenada([1, 2, 3]) retorna True y ordenada(['b', 'a']) retorna False. Desarrollar además un programa para verificar el comportamiento de la función.
+'''
+'''
+def ordenadaAscendente(lista):
+    return lista == sorted(lista)
+
+resultado = ordenadaAscendente([5,4,8,9,1,2])
+print(resultado)
+resultado2 = ordenadaAscendente([1,5,25,98,15631])
+print(resultado2)
+'''
+
+'''
+10) Desarrollar una función que determine si una cadena de caracteres es capicúa, sin utilizar cadenas auxiliares ni rebanadas. Escribir además un programa que permita verificar su funcionamiento.
+'''
+def capicua(cadena): #Tengo que probar hacer un ciclo que recorra todas las posiciones de los caracteres y lo compare contra los opuestos
+    #no funciona esto, después tengo que ver el error
+    #return cadena.split("").join("") == cadena.split("").reversed().join("")
+
+resultado = capicua("hola")
+print(resultado)
+
+'''
+11) Leer una cadena de caracteres e imprimirla centrada en pantalla. Suponer que la misma tiene 80 columnas.
+'''
+
+
+'''
+12) Escribir una función que reciba como parámetro una tupla conteniendo una fecha (día,mes,año) y devuelva una cadena de caracteres con la misma fecha expresada en formato extendido. Por ejemplo, para (12, 10,17) devuelve “12 de Octubre de 2017”. Escribir también un programa para verificar su comportamiento.
+'''
+
+
+'''
+13) Ingresar una frase desde el teclado y usar un conjunto para eliminar las palabras repetidas, dejando un solo ejemplar de cada una. Finalmente mostrar las palabras ordenadas según su longitud.
+'''
+
+
+'''
+14) Desarrollar una función eliminar_claves() que reciba como parámetros un diccionario y una lista de claves. La función debe eliminar del diccionario todas las claves contenidas en la lista, devolviendo el diccionario modificado y un valor de verdad que indique si la operación fue exitosa. Desarrollar también un programa para verificar su comportamiento.
+'''
+
+
+'''
+15) Escribir una función para eliminar una subcadena de una cadena de caracteres, a partir de una posición y cantidad de caracteres dados, devolviendo la cadena resultante. Escribir también un programa para verificar el comportamiento de la misma. Escribir una función utilizando rebanadas.
+'''
