5142933e763360d0bc305b6f2886f083c16cde71
alanpirotta <alanupirotta@gmail.com>
2021-12-03 00:26:37 -0300

    ej2 herencia

--- codoAcodo/ej1_herencia/main.py
+++ codoAcodo/ej1_herencia/main.py
@@ -4,21 +4,27 @@ from camioneta import Camioneta
 from moto import Moto
 
 def main():
-    coche= Coche('rojo', 4, 55, 40)
-    print(coche)
+    # print(coche)
     vehiculos=[
-        coche, 
+        Coche('rojo', 4, 55, 40), 
         Bicicleta('rojo', 4, 'turbina'),
         Camioneta('rojo', 4, 55, 40, 1100),
         Moto('rojo', 4, 'deportiva', 160, 50)
     ]
     #Si imprimo esto directo, me muestra las ubicaciones en memoria
     # print(vehiculos)  
-    print(vehiculos[0])  
+    # print(vehiculos[0])  
 
     return vehiculos
 
+def catalogar(articulos):
+    # for clase, atributos in enumerate(articulos):
+        # print(f'Clase {articulos[clase].__class__.__name__}: \nAtributos: {atributos}')
+    for articulo in articulos:
+        print(f'Clase: {articulo.__class__.__name__}.\nAtributos: {articulo}.')
+#No estoy seguro si está mal que retorne None
+        
     
 if __name__ == '__main__':
-    prueba= main()
-    print(prueba[3])
+    prueba= catalogar(main())
+    print(prueba)
